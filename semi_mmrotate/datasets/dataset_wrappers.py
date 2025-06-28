342014188e8857c1d49e68e90cf0157af26d0113
zw <zwlzt0808@gmail.com>
2025-06-28 22:44:30 +0800

    register sparsedataset for sparse

--- semi_mmrotate/datasets/dataset_wrappers.py
+++ semi_mmrotate/datasets/dataset_wrappers.py
@@ -14,3 +14,23 @@ class SemiDataset(ConcatDataset):
     @property
     def unsup(self):
         return self.datasets[1]
+
+
+@DATASETS.register_module()
+class SparseDataset(ConcatDataset):
+    """Wrapper for semisupervised od."""
+
+    def __init__(self, sup: dict, unsup_unlabeled: dict, unsup_labeled: dict, **kwargs):
+        super().__init__([build_dataset(sup), build_dataset(unsup_unlabeled), build_dataset(unsup_labeled)], **kwargs)
+
+    @property
+    def sup(self):
+        return self.datasets[0]
+
+    @property
+    def unsup_unlabeled(self):
+        return self.datasets[1]
+    
+    @property
+    def unsup_labeled(self):
+        return self.datasets[2]
