488c1f99a5a1c53bec05067e2e152cbacee3dc1b
zw <zwlzt0808@gmail.com>
2025-06-28 22:43:57 +0800

    add sparsedataset for sparse

--- semi_mmrotate/datasets/__init__.py
+++ semi_mmrotate/datasets/__init__.py
@@ -7,3 +7,5 @@ from .pipelines import *
 from .samplers import *
 from .dataset_wrappers import SemiDataset
 from .debug_dota_dataset import DebugDOTADataset
+
+from .dataset_wrappers import SparseDataset
