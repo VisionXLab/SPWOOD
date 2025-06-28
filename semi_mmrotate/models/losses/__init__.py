f3ccc0a6ac5ae517259fc4436bff863bd7091c21
zw <zwlzt0808@gmail.com>
2025-06-28 22:47:34 +0800

    add loss

--- semi_mmrotate/models/losses/__init__.py
+++ semi_mmrotate/models/losses/__init__.py
@@ -5,4 +5,5 @@ from .mcl_loss2 import RotatedMCLLoss2
 from .rotated_arsl_loss import RotatedARSLLoss
 from .point2rboxv2_loss import GaussianOverlapLoss, GaussianVoronoiLoss, EdgeLoss
 from .mcl_loss5 import Semi_GmmLoss
-
+from .rotated_dense_teacher_loss_aa_v3 import RotatedDTLossAssignerAssistentV3
+from .rotated_dense_teacher_loss_aa_v3_for_labeled_data import RotatedDTLossAssignerAssistentV3forLabeledData
