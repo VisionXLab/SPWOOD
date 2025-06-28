b4b7dffd85267cf365f48d4e2945409b36c6a7bf
zw <zwlzt0808@gmail.com>
2025-06-28 22:46:04 +0800

    add dense_head

--- semi_mmrotate/models/dense_heads/__init__.py
+++ semi_mmrotate/models/dense_heads/__init__.py
@@ -4,3 +4,4 @@ from .semi_rotated_retina_head_mcl import SemiRotatedRetinaHeadMCL
 from .semi_rotated_fcos_head_iou import SemiRotatedFCOSHeadIOU
 from .semi_point2rboxv2_head import SemiPoint2RBoxV2Head
 from .semi_mix_head import SemiMixHead
+from .semi_mix_head1 import SemiMixHead1
